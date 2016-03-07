251d6fbb1e22820a665341e89b7eb9f4b2cfd081
Adriana Gonzalez <adrianagonzalez@its-mac-mini-3.mty.ipointsystems.com>
2016-03-07 17:08:50 -0600

    First commit

---
+++ scorePoint/GameVC.swift
@@ -0,0 +1,53 @@
+//
+//  GameVC.swift
+//  scorePoint
+//
+//  Created by Adriana Gonzalez on 3/7/16.
+//  Copyright © 2016 Adriana Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class GameVC: UIViewController {
+
+    @IBOutlet weak var playerOneImg: UIImageView!
+    @IBOutlet weak var playerTwoImg: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+        //let value = UIInterfaceOrientation.LandscapeLeft.rawValue
+        //UIDevice.currentDevice().setValue(value, forKey: "orientation")
+
+        playerOneImg.layer.cornerRadius = playerOneImg.frame.size.width / 2
+        playerOneImg.clipsToBounds = true
+        playerTwoImg.layer.cornerRadius = playerTwoImg.frame.size.width / 2
+        playerTwoImg.clipsToBounds = true
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+//    override func shouldAutorotate() -> Bool {
+//        return true
+//    }
+
+    @IBAction func closeBtnPressed(sender: AnyObject) {
+    
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
